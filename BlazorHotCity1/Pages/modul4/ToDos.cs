8c34a92aebb76b1f7a430ad3c951f6be65286266
Hannes Preishuber <hannesp@ppedv.de>
2022-06-29 16:01:28 +0200

    REST Call HttpClient - Asnyc Task für automatisches "StateHaschanged"

---
+++ BlazorHotCity1/Pages/modul4/ToDos.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorHotCity1.Pages.modul4
+{
+
+    
+
+    public class Todos
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
