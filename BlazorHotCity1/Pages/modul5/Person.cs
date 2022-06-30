59a78e4d0027397c0061c51e20c5930c5f6bc070
Hannes Preishuber <hannesp@ppedv.de>
2022-06-30 14:03:09 +0200

    Editform

---
+++ BlazorHotCity1/Pages/modul5/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorHotCity1.Pages.modul5
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="Bitte ID")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Bitte Name")]
+        [StringLength(10,ErrorMessage ="zu lange")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Bitte Gross")]
+        [Range(140,199, ErrorMessage ="keine Achterbahn")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage = "Bitte GebDat")]
+        public DateTime GebDat { get; set; }
+    }
+}
