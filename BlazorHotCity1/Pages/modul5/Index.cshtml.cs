a3233db853969cff0dcd8045f06cb0f13620a52d
Hannes Preishuber <hannesp@ppedv.de>
2022-06-30 13:34:10 +0200

    HTML Formular

---
+++ BlazorHotCity1/Pages/modul5/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorHotCity1.Pages.modul5
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        public void OnPost()
+        {
+        }
+    }
+}
