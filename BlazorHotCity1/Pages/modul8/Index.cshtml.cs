f06f2308eb2fcd21b87e98210825db0e201ebdd1
Hannes Preishuber <hannesp@ppedv.de>
2022-07-01 16:10:07 +0200

    JavaScript Snippets

---
+++ BlazorHotCity1/Pages/modul8/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorHotCity1.Pages.modul8
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
